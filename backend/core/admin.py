3caeace10498f884ddb3e265c32e534a2a7bfeac
Amina <aminayegnb@gmail.com>
2025-04-22 15:21:31 +0500

    Deleting unnecessary files

--- backend/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, TariffPlan, UserTariffPlan, Transaction
-
-admin.site.register(User)
-admin.site.register(TariffPlan)
-admin.site.register(UserTariffPlan)
-admin.site.register(Transaction)
